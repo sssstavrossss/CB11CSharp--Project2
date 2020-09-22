701881059ff358e9ade76ed36f4c3b1d5c312234
sssstavrossss <vasileiadis.sta@gmail.com>
2020-09-22 22:32:27 +0300

    initiating

---
+++ Web/README.md
@@ -0,0 +1,2 @@
+# CB11CSharp-Project2
+Individual Project 2 - Stavros Vasileiadis - Coding Bootcamp AFDEMP Peoplecert
---
+++ Web/assets/js/custom.js
@@ -0,0 +1,9 @@
+﻿/* Set the width of the side navigation to 220px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "220px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
