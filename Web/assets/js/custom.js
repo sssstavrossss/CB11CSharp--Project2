60d1940e607a308ee84a3cd62907ca7a426abbb9
sssstavrossss <vasileiadis.sta@gmail.com>
2020-10-02 02:43:52 +0300

    updating jquerry

--- Web/assets/js/custom.js
+++ Web/assets/js/custom.js
@@ -14,7 +14,6 @@ function Data () {
             this.Assignments = Assignments;
         },
         Entities: [],
-        Return: function () { return this.Entities }, 
         Keys: ["ID", "First Name", "Last Name", "Birthdate", "Tuition Fees", "Courses", "Assignments"]
     };
 
@@ -31,7 +30,6 @@ function Data () {
             this.Trainers = Trainers;
         },
         Entities: [],
-        Return: function () { return this.Entities },
         Keys: ["ID", "Title", "Stream", "Type", "Start Date", "End Date", "Students", "Assignments", "Trainers"]
     };
 
@@ -44,7 +42,6 @@ function Data () {
             this.Courses = Courses;
         },
         Entities: [],
-        Return: function () { return this.Entities },
         Keys: ["ID", "First Name", "Last Name", "Subject", "Courses"]
     };
 
@@ -58,186 +55,124 @@ function Data () {
             this.Students = Students;
         },
         Entities: [],
-        Return: function () { return this.Entities },
         Keys: ["ID", "Title", "Description", "Submission Date", "Students", "Courses"]
     };
 
     (function () {
-        Students.Entities.push(new Students.Constructor(1, "Stavros", "Vasileiadis", 1991, 2000, [], []));
-        Students.Entities.push(new Students.Constructor(2, "Johann Sebastian", "Bach", 1986, 2500, [], []));
-        Students.Entities.push(new Students.Constructor(3, "Nikolaos", "Skalkotas", 1977, 2100, [], []));
-        Students.Entities.push(new Students.Constructor(4, "Giacinto", "Scelsi", 1955, 2400, [], []));
-        Students.Entities.push(new Students.Constructor(5, "Edgard", "Varese", 1988, 3000, [], []));
-        Students.Entities.push(new Students.Constructor(6, "Amadeus", "Mozart", 1991, 2000, [], []));
-        Students.Entities.push(new Students.Constructor(7, "Ludwig", "van Beethoven", 1992, 2500, [], []));
-        Students.Entities.push(new Students.Constructor(8, "Johann", "Stauess", 1999, 3000, [], []));
-        Students.Entities.push(new Students.Constructor(9, "Igor", "Stravisky", 1974, 2100, [], []));
-        Students.Entities.push(new Students.Constructor(10, "Krzysztof", "Penderecki", 1985, 2400, [], []));
-        Courses.Entities.push(new Courses.Constructor(1, "C#", "Programming", "Part time", "01/02/2021", "01/08/2021", [], [], []));
-        Courses.Entities.push(new Courses.Constructor(2, "Go", "Programming", "Full time", "05/03/2021", "15/06/2021", [], [], []));
-        Courses.Entities.push(new Courses.Constructor(3, "Java", "Programming", "Full time", "05/03/2021", "15/06/2021", [], [], []));
-        Trainers.Entities.push(new Trainers.Constructor(1, "Ioannes", "Philippidis", "Back End", []));
-        Trainers.Entities.push(new Trainers.Constructor(2, "Constantine", "Palaiologos", "Front End", []));
-        Trainers.Entities.push(new Trainers.Constructor(3, "Yorgo", "Papadopoulos", "Back End", []));
-        Assignments.Entities.push(new Assignments.Constructor(1, "Invididual Project 1", "Individual Project 1 Description", "30/06/2021", [], []));
-        Assignments.Entities.push(new Assignments.Constructor(2, "Idividual Project 2", "Individual Project 2 Description", "15/05/2021", []));
-        Assignments.Entities.push(new Assignments.Constructor(3, "Team Project 1", "Team Project 1 Description", "15/06/2021", [], []));
-        Assignments.Entities.push(new Assignments.Constructor(4, "Team Project 2", "Tean Project 2 Description", "01/08/2021", [], []));
-        Assignments.Entities.push(new Assignments.Constructor(5, "Individual Project 3", "Individual Project 3 Description", "05/04/2021", [], []));
-    })();
-
-    (function () {
-        Students.Entities[0].Courses.push(Courses.Entities[0]);
-        Students.Entities[0].Assignments.push(Assignments.Entities[0]);
-        Students.Entities[0].Assignments.push(Assignments.Entities[2]);
-        Students.Entities[1].Courses.push(Courses.Entities[0]);
-        Students.Entities[1].Assignments.push(Assignments.Entities[0]);
-        Students.Entities[1].Assignments.push(Assignments.Entities[1]);
-        Students.Entities[2].Courses.push(Courses.Entities[1]);
-        Students.Entities[2].Assignments.push(Assignments.Entities[2]);
-        Students.Entities[3].Courses.push(Courses.Entities[1]);
-        Students.Entities[3].Assignments.push(Assignments.Entities[3]);
-        Students.Entities[3].Assignments.push(Assignments.Entities[4]);
-        Students.Entities[4].Courses.push(Courses.Entities[0]);
-        Students.Entities[4].Courses.push(Courses.Entities[1]);
-        Students.Entities[4].Assignments.push(Assignments.Entities[1]);
-        Students.Entities[5].Courses.push(Courses.Entities[1]);
-        Students.Entities[5].Courses.push(Courses.Entities[0]);
-        Students.Entities[5].Assignments.push(Assignments.Entities[4]);
-        Students.Entities[5].Assignments.push(Assignments.Entities[0]);
-        Students.Entities[6].Courses.push(Courses.Entities[0]);
-        Students.Entities[6].Assignments.push(Assignments.Entities[0]);
-        Students.Entities[6].Assignments.push(Assignments.Entities[2]);
-        Students.Entities[6].Assignments.push(Assignments.Entities[3]);
-        Students.Entities[7].Courses.push(Courses.Entities[1]);
-        Students.Entities[7].Assignments.push(Assignments.Entities[4]);
-        Students.Entities[8].Courses.push(Courses.Entities[0]);
-        Students.Entities[8].Assignments.push(Assignments.Entities[3]);
-        Students.Entities[9].Courses.push(Courses.Entities[0]);
-        Students.Entities[9].Assignments.push(Assignments.Entities[2]);
+        Students.Entities.push(new Students.Constructor(1, "Stavros", "Vasileiadis", 1991, 2000, [1], [2,3]));
+        Students.Entities.push(new Students.Constructor(2, "Johann Sebastian", "Bach", 1986, 2500, [2], [1,4]));
+        Students.Entities.push(new Students.Constructor(3, "Nikolaos", "Skalkotas", 1977, 2100, [3], [2,4]));
+        Students.Entities.push(new Students.Constructor(4, "Giacinto", "Scelsi", 1955, 2400, [1,2], [2,3]));
+        Students.Entities.push(new Students.Constructor(5, "Edgard", "Varese", 1988, 3000, [2], [1,3,4]));
+        Students.Entities.push(new Students.Constructor(6, "Amadeus", "Mozart", 1991, 2000, [3], [1,2]));
+        Students.Entities.push(new Students.Constructor(7, "Ludwig", "van Beethoven", 1992, 2500, [2,3], [2,3]));
+        Students.Entities.push(new Students.Constructor(8, "Johann", "Stauess", 1999, 3000, [1,2], [1,4]));
+        Students.Entities.push(new Students.Constructor(9, "Igor", "Stravisky", 1974, 2100, [1,3], [2,4]));
+        Students.Entities.push(new Students.Constructor(10, "Krzysztof", "Penderecki", 1985, 2400, [1], [1,5]));
+        Courses.Entities.push(new Courses.Constructor(1, "C#", "Programming", "Part time", "01/02/2021", "01/08/2021", [1,4,8,9,10], [2,3,1,5], [1]));
+        Courses.Entities.push(new Courses.Constructor(2, "Go", "Programming", "Full time", "05/03/2021", "15/06/2021", [2,4,5,7,8], [1,4,2,3], [1,3]));
+        Courses.Entities.push(new Courses.Constructor(3, "Java", "Programming", "Full time", "05/03/2021", "15/06/2021", [3,6,7,9], [2,4,1,3], [2]));
+        Trainers.Entities.push(new Trainers.Constructor(1, "Ioannes", "Philippidis", "Back End", [1,2]));
+        Trainers.Entities.push(new Trainers.Constructor(2, "Constantine", "Palaiologos", "Front End", [3]));
+        Trainers.Entities.push(new Trainers.Constructor(3, "Yorgo", "Papadopoulos", "Back End", [2]));
+        Assignments.Entities.push(new Assignments.Constructor(1, "Invididual Project 1", "Individual Project 1 Description", "30/06/2021", [1,2,3], [2,5,6,8,10]));
+        Assignments.Entities.push(new Assignments.Constructor(2, "Idividual Project 2", "Individual Project 2 Description", "15/05/2021", [1,2,3], [1,3,4,6,7,9]));
+        Assignments.Entities.push(new Assignments.Constructor(3, "Team Project 1", "Team Project 1 Description", "15/06/2021", [1,2,3], [1,4,5,7]));
+        Assignments.Entities.push(new Assignments.Constructor(4, "Team Project 2", "Tean Project 2 Description", "01/08/2021", [2,3], [2,3,5,8,9]));
+        Assignments.Entities.push(new Assignments.Constructor(5, "Individual Project 3", "Individual Project 3 Description", "05/04/2021", [5], [10]));
     })();
     
-    return [Students , Courses , Trainers , Assignments ]; 
+    return [ { Students }, { Courses }, { Trainers }, { Assignments } ]; 
 
 }
 
 //table creation self invoked funtion
 (function (Data) {
 
-    var HeaderHtml = `<i class="icofont-plus-circle icofont-2x" title="add"></i>`;
-    var BodyHtml = `<i class="icofont-gear icofont-2x" title="edit"></i>
-                    <i class="icofont-bin icofont-2x" title="remove"></i>`;
+    console.log(Data);
 
-    //var Key = ["Trainers","Assignments","Students","Courses"];
+    var HeaderHtml = `<th><i class="icofont-plus-circle icofont-2x" title="add"></i></th>`;
+    var BodyHtml = `<td><i class="icofont-gear icofont-2x" title="edit"></i>
+                    <i class="icofont-bin icofont-2x" title="remove"></i></td>`;
 
-    var i = 0;
 
-    for (var Dt of Data) {
+    jQuery.map(Data, function (Obj, i) { CreateTable(Obj) });
 
-        //console.log(Object.keys(Dt.Entities[0]));
+    function CreateTable(Obj) {
 
-        var table = document.createElement('table');
+        var objkey = Object.keys(Obj);
+        var divid = `#table_div_${objkey}`;
+        var tableid = `table_data_${objkey}`;
+        var div = $(divid);
 
-        if (i == 0) {
-            var div = document.getElementById('table_div_Students');
-            table.setAttribute('id', 'table_data_Students');
-        } else if (i == 1) {
-            var div = document.getElementById('table_div_Courses');
-            table.setAttribute('id', 'table_data_Courses');
-        } else if (i == 2) {
-            var div = document.getElementById('table_div_Trainers');
-            table.setAttribute('id', 'table_data_Trainers');
-        } else if (i == 3) {
-            var div = document.getElementById('table_div_Assignments');
-            table.setAttribute('id', 'table_data_Assignments');
+        var table = $('<table>');
+        table.attr('id', tableid);
+
+        //CREATE HEAD
+        var tharray = CreateTHeadTh(); //to create an array of th elements and append them to thead tr
+
+        var thead = $('<thead>')
+            .append($('<tr>')
+                .append([
+                    tharray,
+                    HeaderHtml
+                ]));
+
+        function CreateTHeadTh() {
+            var ar = [];
+            jQuery.map(Obj[objkey].Keys, function (v, i) { ar.push(`<th>${v}</th>`) });
+            return ar;
         }
+
+        //CREATE BODY
+
+        var trarray = CreateTBodyTr(); //to create an array of tr elements and append them to tbody
+
+        var tbody = $('<tbody>')
+            .append([
+                trarray
+            ]);
+
+        //console.log(Obj[objkey].Entities);
+
+        function CreateTBodyTr() {
+            var ar = [];
+            for (var item of Obj[objkey].Entities) {
+                //console.log(item);
+                //console.log(Object.keys(item));
+                ar.push(`<tr>${CreateTBodyTh(item, Object.keys(item))}</tr>`);
+            }
+            return ar;
+        }
+
+        function CreateTBodyTh(item, key) {
+            var ar = [];
+            jQuery.map(item, function (v, i) {
+                if (jQuery.isArray(v)) {
+                    ar.push(`<td><ul class="no-bullets">${v}</ul></td>`);
+                } else {
+                    ar.push(`<td>${v}</td>`);
+                }
+            });
+            ar.push(BodyHtml);
+            return ar;
+        }
+
+        function GetUl(item) {
+            var li;
+            for (var val of item) {
+                console.log(val);
+                li += `<li>${val}</li>`;
+            }
+            return li;
+        }
+
+        table.append([
+            thead,
+            tbody
+        ]);
+        div.append(table);
         
-        //var n = Object.keys(Dt);
-        //console.log(Dt.Keys);
-
-        (function () {
-            var thead = document.createElement('thead');
-            var tr = document.createElement('tr');
-            for (var item of Dt.Keys) {
-                var th = document.createElement('th');
-                var thtext = document.createTextNode(item);
-                th.appendChild(thtext);
-                tr.appendChild(th);
-            }
-            var thLast = document.createElement('th');
-            tr.appendChild(thLast);
-            thead.appendChild(tr);
-            table.appendChild(thead);
-            thLast.innerHTML = HeaderHtml;
-        })();
-
-        //var st = Object.keys(Data[0]);
-
-        (function () {
-            var tbody = document.createElement('tbody');
-            var EntitiesKeys = Object.keys(Dt.Entities[0]);
-            for (var item of Dt.Entities) {
-               // console.log(item['ID']);
-                (function () {
-                    var tr = document.createElement('tr');
-                    for (var item2 of EntitiesKeys) {
-                        //console.log(item[item2]);
-                        var text = "";
-                        if (Array.isArray(item[item2])) {
-                           //console.log(1);
-                            var ul = document.createElement('ul')
-                            for (var item3 of item[item2]) {
-                                if (item3.title) {
-                                    var li = document.createElement('li');
-                                    var litext = document.createTextNode(item3.title);
-                                    li.appendChild(litext);
-                                }
-                                if (item3.firstName) {
-                                    text = text + item.fistName + " " + item.lastName;
-                                }
-                                ul.appendChild(li);
-                            }
-                        } else {
-                            //console.log(2);
-                            text = item[item2];
-
-                        }
-                        if (text) {
-                            var td = document.createElement('td');
-                            var tdtext = document.createTextNode(text);
-                            td.appendChild(tdtext);
-                            tr.appendChild(td);
-                        }
-                        if (ul) {
-                            ul.classList.add('no-bullets');
-                            var td = document.createElement('td');
-                            td.appendChild(ul);
-                            tr.appendChild(td);
-                        }
-                    }
-                    var tdLast = document.createElement('td');
-                    tr.appendChild(tdLast);
-                    tbody.appendChild(tr);
-                    table.appendChild(tbody);
-                    tdLast.innerHTML = BodyHtml;
-                    tr.setAttribute('data-id', item.ID);
-                    if (i === 0) {
-                        tr.setAttribute('data-type', 'Students');
-                    } else if (i === 1) {
-                        tr.setAttribute('data-type', 'Courses');
-                    } else if (i === 2) {
-                        tr.setAttribute('data-type', 'Trainers');
-                    } else if (i === 3) {
-                        tr.setAttribute('data-type', 'Assignments');
-                    }
-                })();
-            }
-            table.appendChild(tbody);
-        })();
-        //console.log(div);
-
-        div.appendChild(table);
-        i++;
     }
 
 })(Data());
