90141856d6fab0935484fce4e8f6812b4637b12a
sssstavrossss <vasileiadis.sta@gmail.com>
2020-10-03 00:13:58 +0300

    jquery overhaul

--- Web/assets/js/custom.js
+++ Web/assets/js/custom.js
@@ -1,7 +1,7 @@
 ﻿//Dataset
 
 //Main data function with objects and initiation of objects
-function Data () {
+function Data() {
 
     var Students = {
         Constructor: function Students(SID, firstName, lastName, dateOfBirth, tuitionFees, Courses, Assignments) {
@@ -59,56 +59,158 @@ function Data () {
     };
 
     (function () {
-        Students.Entities.push(new Students.Constructor(1, "Stavros", "Vasileiadis", 1991, 2000, [1], [2,3]));
-        Students.Entities.push(new Students.Constructor(2, "Johann Sebastian", "Bach", 1986, 2500, [2], [1,4]));
-        Students.Entities.push(new Students.Constructor(3, "Nikolaos", "Skalkotas", 1977, 2100, [3], [2,4]));
-        Students.Entities.push(new Students.Constructor(4, "Giacinto", "Scelsi", 1955, 2400, [1,2], [2,3]));
-        Students.Entities.push(new Students.Constructor(5, "Edgard", "Varese", 1988, 3000, [2], [1,3,4]));
-        Students.Entities.push(new Students.Constructor(6, "Amadeus", "Mozart", 1991, 2000, [3], [1,2]));
-        Students.Entities.push(new Students.Constructor(7, "Ludwig", "van Beethoven", 1992, 2500, [2,3], [2,3]));
-        Students.Entities.push(new Students.Constructor(8, "Johann", "Stauess", 1999, 3000, [1,2], [1,4]));
-        Students.Entities.push(new Students.Constructor(9, "Igor", "Stravisky", 1974, 2100, [1,3], [2,4]));
-        Students.Entities.push(new Students.Constructor(10, "Krzysztof", "Penderecki", 1985, 2400, [1], [1,5]));
-        Courses.Entities.push(new Courses.Constructor(1, "C#", "Programming", "Part time", "01/02/2021", "01/08/2021", [1,4,8,9,10], [2,3,1,5], [1]));
-        Courses.Entities.push(new Courses.Constructor(2, "Go", "Programming", "Full time", "05/03/2021", "15/06/2021", [2,4,5,7,8], [1,4,2,3], [1,3]));
-        Courses.Entities.push(new Courses.Constructor(3, "Java", "Programming", "Full time", "05/03/2021", "15/06/2021", [3,6,7,9], [2,4,1,3], [2]));
-        Trainers.Entities.push(new Trainers.Constructor(1, "Ioannes", "Philippidis", "Back End", [1,2]));
-        Trainers.Entities.push(new Trainers.Constructor(2, "Constantine", "Palaiologos", "Front End", [3]));
-        Trainers.Entities.push(new Trainers.Constructor(3, "Yorgo", "Papadopoulos", "Back End", [2]));
-        Assignments.Entities.push(new Assignments.Constructor(1, "Invididual Project 1", "Individual Project 1 Description", "30/06/2021", [1,2,3], [2,5,6,8,10]));
-        Assignments.Entities.push(new Assignments.Constructor(2, "Idividual Project 2", "Individual Project 2 Description", "15/05/2021", [1,2,3], [1,3,4,6,7,9]));
-        Assignments.Entities.push(new Assignments.Constructor(3, "Team Project 1", "Team Project 1 Description", "15/06/2021", [1,2,3], [1,4,5,7]));
-        Assignments.Entities.push(new Assignments.Constructor(4, "Team Project 2", "Tean Project 2 Description", "01/08/2021", [2,3], [2,3,5,8,9]));
-        Assignments.Entities.push(new Assignments.Constructor(5, "Individual Project 3", "Individual Project 3 Description", "05/04/2021", [5], [10]));
+        Students.Entities.push(new Students.Constructor(1, "Stavros", "Vasileiadis", 1991, 2000, [], []));
+        Students.Entities.push(new Students.Constructor(2, "Johann Sebastian", "Bach", 1986, 2500, [], []));
+        Students.Entities.push(new Students.Constructor(3, "Nikolaos", "Skalkotas", 1977, 2100, [], []));
+        Students.Entities.push(new Students.Constructor(4, "Giacinto", "Scelsi", 1955, 2400, [], []));
+        Students.Entities.push(new Students.Constructor(5, "Edgard", "Varese", 1988, 3000, [], []));
+        Students.Entities.push(new Students.Constructor(6, "Amadeus", "Mozart", 1991, 2000, [], []));
+        Students.Entities.push(new Students.Constructor(7, "Ludwig", "van Beethoven", 1992, 2500, [], []));
+        Students.Entities.push(new Students.Constructor(8, "Johann", "Stauess", 1999, 3000, [], []));
+        Students.Entities.push(new Students.Constructor(9, "Igor", "Stravisky", 1974, 2100, [], []));
+        Students.Entities.push(new Students.Constructor(10, "Krzysztof", "Penderecki", 1985, 2400, [], []));
+        Courses.Entities.push(new Courses.Constructor(1, "C#", "Programming", "Part time", "01/02/2021", "01/08/2021", [], [], []));
+        Courses.Entities.push(new Courses.Constructor(2, "Go", "Programming", "Full time", "05/03/2021", "15/06/2021", [], [], []));
+        Courses.Entities.push(new Courses.Constructor(3, "Java", "Programming", "Full time", "05/03/2021", "15/06/2021", [], [], []));
+        Trainers.Entities.push(new Trainers.Constructor(1, "Ioannes", "Philippidis", "Back End", []));
+        Trainers.Entities.push(new Trainers.Constructor(2, "Constantine", "Palaiologos", "Front End", []));
+        Trainers.Entities.push(new Trainers.Constructor(3, "Yorgo", "Papadopoulos", "Back End", []));
+        Assignments.Entities.push(new Assignments.Constructor(1, "Invididual Project 1", "Individual Project 1 Description", "30/06/2021", [], []));
+        Assignments.Entities.push(new Assignments.Constructor(2, "Idividual Project 2", "Individual Project 2 Description", "15/05/2021", [], []));
+        Assignments.Entities.push(new Assignments.Constructor(3, "Team Project 1", "Team Project 1 Description", "15/06/2021", [], []));
+        Assignments.Entities.push(new Assignments.Constructor(4, "Team Project 2", "Tean Project 2 Description", "01/08/2021", [], []));
+        Assignments.Entities.push(new Assignments.Constructor(5, "Individual Project 3", "Individual Project 3 Description", "05/04/2021", [], []));
+        Students.Entities[0].Courses.push(Courses.Entities[0]);
+        Students.Entities[0].Assignments.push(Assignments.Entities[1]);
+        Students.Entities[0].Assignments.push(Assignments.Entities[2]);
+        Students.Entities[1].Courses.push(Courses.Entities[1]);
+        Students.Entities[1].Assignments.push(Assignments.Entities[0]);
+        Students.Entities[1].Assignments.push(Assignments.Entities[3]);
+        Students.Entities[2].Courses.push(Courses.Entities[2]);
+        Students.Entities[2].Assignments.push(Assignments.Entities[1]);
+        Students.Entities[2].Assignments.push(Assignments.Entities[3]);
+        Students.Entities[3].Courses.push(Courses.Entities[0]);
+        Students.Entities[3].Courses.push(Courses.Entities[1]);
+        Students.Entities[3].Assignments.push(Assignments.Entities[1]);
+        Students.Entities[3].Assignments.push(Assignments.Entities[2]);
+        Students.Entities[4].Courses.push(Courses.Entities[1]);
+        Students.Entities[4].Assignments.push(Assignments.Entities[0]);
+        Students.Entities[4].Assignments.push(Assignments.Entities[2]);
+        Students.Entities[4].Assignments.push(Assignments.Entities[3]);
+        Students.Entities[5].Courses.push(Courses.Entities[2]);
+        Students.Entities[5].Assignments.push(Assignments.Entities[0]);
+        Students.Entities[5].Assignments.push(Assignments.Entities[1]);
+        Students.Entities[6].Courses.push(Courses.Entities[1]);
+        Students.Entities[6].Courses.push(Courses.Entities[2]);
+        Students.Entities[6].Assignments.push(Assignments.Entities[1]);
+        Students.Entities[6].Assignments.push(Assignments.Entities[2]);
+        Students.Entities[7].Courses.push(Courses.Entities[0]);
+        Students.Entities[7].Courses.push(Courses.Entities[1]);
+        Students.Entities[7].Assignments.push(Assignments.Entities[0]);
+        Students.Entities[7].Assignments.push(Assignments.Entities[3]);
+        Students.Entities[8].Courses.push(Courses.Entities[0]);
+        Students.Entities[8].Courses.push(Courses.Entities[2]);
+        Students.Entities[8].Assignments.push(Assignments.Entities[1]);
+        Students.Entities[8].Assignments.push(Assignments.Entities[3]);
+        Students.Entities[9].Courses.push(Courses.Entities[0]);
+        Students.Entities[9].Assignments.push(Assignments.Entities[0]);
+        Students.Entities[9].Assignments.push(Assignments.Entities[4]);
+        Courses.Entities[0].Students.push(Students.Entities[0]);
+        Courses.Entities[0].Students.push(Students.Entities[3]);
+        Courses.Entities[0].Students.push(Students.Entities[7]);
+        Courses.Entities[0].Students.push(Students.Entities[8]);
+        Courses.Entities[0].Students.push(Students.Entities[9]);
+        Courses.Entities[0].Assignments.push(Assignments.Entities[0]);
+        Courses.Entities[0].Assignments.push(Assignments.Entities[1]);
+        Courses.Entities[0].Assignments.push(Assignments.Entities[2]);
+        Courses.Entities[0].Assignments.push(Assignments.Entities[4]);
+        Courses.Entities[0].Trainers.push(Trainers.Entities[0]);
+        Courses.Entities[1].Students.push(Students.Entities[1]);
+        Courses.Entities[1].Students.push(Students.Entities[3]);
+        Courses.Entities[1].Students.push(Students.Entities[4]);
+        Courses.Entities[1].Students.push(Students.Entities[6]);
+        Courses.Entities[1].Students.push(Students.Entities[7]);
+        Courses.Entities[1].Assignments.push(Assignments.Entities[0]);
+        Courses.Entities[1].Assignments.push(Assignments.Entities[1]);
+        Courses.Entities[1].Assignments.push(Assignments.Entities[2]);
+        Courses.Entities[1].Assignments.push(Assignments.Entities[3]);
+        Courses.Entities[1].Trainers.push(Trainers.Entities[0]);
+        Courses.Entities[1].Trainers.push(Trainers.Entities[2]);
+        Courses.Entities[2].Students.push(Students.Entities[2]);
+        Courses.Entities[2].Students.push(Students.Entities[5]);
+        Courses.Entities[2].Students.push(Students.Entities[6]);
+        Courses.Entities[2].Students.push(Students.Entities[8]);
+        Courses.Entities[2].Assignments.push(Assignments.Entities[0]);
+        Courses.Entities[2].Assignments.push(Assignments.Entities[2]);
+        Courses.Entities[2].Assignments.push(Assignments.Entities[3]);
+        Courses.Entities[2].Assignments.push(Assignments.Entities[4]);
+        Courses.Entities[2].Trainers.push(Trainers.Entities[1]);
+        Trainers.Entities[0].Courses.push(Courses.Entities[0]);
+        Trainers.Entities[0].Courses.push(Courses.Entities[1]);
+        Trainers.Entities[1].Courses.push(Courses.Entities[2]);
+        Trainers.Entities[2].Courses.push(Courses.Entities[1]);
+        Assignments.Entities[0].Courses.push(Courses.Entities[0]);
+        Assignments.Entities[0].Courses.push(Courses.Entities[1]);
+        Assignments.Entities[0].Courses.push(Courses.Entities[2]);
+        Assignments.Entities[0].Students.push(Students.Entities[1]);
+        Assignments.Entities[0].Students.push(Students.Entities[4]);
+        Assignments.Entities[0].Students.push(Students.Entities[5]);
+        Assignments.Entities[0].Students.push(Students.Entities[7]);
+        Assignments.Entities[0].Students.push(Students.Entities[9]);
+        Assignments.Entities[1].Courses.push(Courses.Entities[0]);
+        Assignments.Entities[1].Courses.push(Courses.Entities[1]);
+        Assignments.Entities[1].Courses.push(Courses.Entities[2]);
+        Assignments.Entities[1].Students.push(Students.Entities[0]);
+        Assignments.Entities[1].Students.push(Students.Entities[2]);
+        Assignments.Entities[1].Students.push(Students.Entities[3]);
+        Assignments.Entities[1].Students.push(Students.Entities[5]);
+        Assignments.Entities[1].Students.push(Students.Entities[6]);
+        Assignments.Entities[1].Students.push(Students.Entities[8]);
+        Assignments.Entities[2].Courses.push(Courses.Entities[0]);
+        Assignments.Entities[2].Courses.push(Courses.Entities[1]);
+        Assignments.Entities[2].Courses.push(Courses.Entities[2]);
+        Assignments.Entities[2].Students.push(Students.Entities[0]);
+        Assignments.Entities[2].Students.push(Students.Entities[3]);
+        Assignments.Entities[2].Students.push(Students.Entities[4]);
+        Assignments.Entities[2].Students.push(Students.Entities[6]);
+        Assignments.Entities[3].Courses.push(Courses.Entities[1]);
+        Assignments.Entities[3].Courses.push(Courses.Entities[2]);
+        Assignments.Entities[3].Students.push(Students.Entities[1]);
+        Assignments.Entities[3].Students.push(Students.Entities[2]);
+        Assignments.Entities[3].Students.push(Students.Entities[4]);
+        Assignments.Entities[3].Students.push(Students.Entities[7]);
+        Assignments.Entities[3].Students.push(Students.Entities[9]);
+        Assignments.Entities[4].Courses.push(Courses.Entities[2]);
+        Assignments.Entities[4].Students.push(Students.Entities[9]);
     })();
-    
-    return [ { Students }, { Courses }, { Trainers }, { Assignments } ]; 
+
+    return { Students: Students, Courses: Courses, Trainers: Trainers, Assignments: Assignments }; 
 
 }
 
 //table creation self invoked funtion
 (function (Data) {
 
-    console.log(Data);
-
     var HeaderHtml = `<th><i class="icofont-plus-circle icofont-2x" title="add"></i></th>`;
     var BodyHtml = `<td><i class="icofont-gear icofont-2x" title="edit"></i>
                     <i class="icofont-bin icofont-2x" title="remove"></i></td>`;
 
+    jQuery.map(Data, function (value, index) { CreateTable(value, index) });
 
-    jQuery.map(Data, function (Obj, i) { CreateTable(Obj) });
+    function CreateTable(DtValue, DtIndex) {
 
-    function CreateTable(Obj) {
-
-        var objkey = Object.keys(Obj);
-        var divid = `#table_div_${objkey}`;
-        var tableid = `table_data_${objkey}`;
+        var divid = `#table_div_${DtIndex}`;
+        var tableid = `table_data_${DtIndex}`;
         var div = $(divid);
 
+        //Create Header
+        var header = `<h2 class="pt-1">Manage ${DtIndex} Data</h2>`;
+
+        //Initiate Table
         var table = $('<table>');
         table.attr('id', tableid);
 
-        //CREATE HEAD
+        //Create Head
         var tharray = CreateTHeadTh(); //to create an array of th elements and append them to thead tr
 
         var thead = $('<thead>')
@@ -120,11 +222,9 @@ function Data () {
 
         function CreateTHeadTh() {
             var ar = [];
-            jQuery.map(Obj[objkey].Keys, function (v, i) { ar.push(`<th>${v}</th>`) });
+            jQuery.map(DtValue.Keys, function (v) { ar.push(`<th>${v}</th>`) });
             return ar;
         }
-
-        //CREATE BODY
 
         var trarray = CreateTBodyTr(); //to create an array of tr elements and append them to tbody
 
@@ -133,36 +233,39 @@ function Data () {
                 trarray
             ]);
 
-        //console.log(Obj[objkey].Entities);
-
         function CreateTBodyTr() {
             var ar = [];
-            for (var item of Obj[objkey].Entities) {
-                //console.log(item);
-                //console.log(Object.keys(item));
-                ar.push(`<tr>${CreateTBodyTh(item, Object.keys(item))}</tr>`);
-            }
+            jQuery.map(DtValue.Entities, function (val) { ar.push(`<tr>${CreateTBodyTrTd(val)}</tr>`) });
             return ar;
         }
 
-        function CreateTBodyTh(item, key) {
+        function CreateTBodyTrTd(val) {
             var ar = [];
-            jQuery.map(item, function (v, i) {
-                if (jQuery.isArray(v)) {
-                    ar.push(`<td><ul class="no-bullets">${v}</ul></td>`);
-                } else {
-                    ar.push(`<td>${v}</td>`);
-                }
-            });
+            jQuery.map(val, function (v, i) { ar.push(`<td>${GetTBodyTrTd(v, i)}</td>`) });
             ar.push(BodyHtml);
             return ar;
         }
 
-        function GetUl(item) {
-            var li;
-            for (var val of item) {
-                console.log(val);
-                li += `<li>${val}</li>`;
+        function GetTBodyTrTd(v, i) {
+            var td = "";
+            if (jQuery.isArray(v)) {
+                td += `<ul class="no-bullets">${GetTBodyTrTdUl(v, i)}</ul>`;
+            } else {
+                td = v;
+            }
+            return td;
+        }
+
+        function GetTBodyTrTdUl(v, i) {
+            var li = "";
+            if (i === "Courses" || i === "Assignments") {
+                for (var item of v) {
+                    li += `<li>${item.title}</li>`;
+                }
+            } else {
+                for (var item of v) {
+                    li += `<li>${item.firstName + " " + item.lastName}</li>`;
+                }
             }
             return li;
         }
@@ -171,8 +274,11 @@ function Data () {
             thead,
             tbody
         ]);
-        div.append(table);
-        
+
+        div.append([
+            header,
+            table]);
+
     }
 
 })(Data());
